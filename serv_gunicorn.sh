252945612a02e58f4a1083fa761aa644486757a8
7xxxx <7xxxx@users.noreply.github.com>
2021-06-16 14:21:07 +0200

    Revert "gunicorn server fix"
    This reverts commit 9e8b979b

--- serv_gunicorn.sh
+++ serv_gunicorn.sh
@@ -3,13 +3,9 @@
 #call this script with parameter kill to terminate the daemons
 
 if [ $# -eq 0 ]; then
-#  "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:443 --certfile=cert/bookstore.pem --key=cert/bookstore.key "app:create_app()"
-#  "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:80 "app:create_app()"
-  gunicorn -D -w 2 --bind 0.0.0.0:443 --certfile=cert/bookstore.pem --key=cert/bookstore.key "app:create_app()"
-  gunicorn -D -w 2 --bind 0.0.0.0:80 "app:create_app()"
+  "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:443 --certfile=cert/bookstore.pem --key=cert/bookstore.key "app:create_app()"
+  "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:80 "app:create_app()"
 fi
-
-sleep 1
 
 if [ "$1" = "kill" ] ; then
   for pid in $(pgrep gunicorn) ; do

ba4951935f9ffc76cc1bb515fd156332323549a9
7xxxx <7xxxx@users.noreply.github.com>
2021-08-05 17:19:01 +0200

    random secret_key on each server start

--- serv_gunicorn.sh
+++ serv_gunicorn.sh
@@ -3,6 +3,8 @@
 #call this script with parameter kill to terminate the daemons
 
 if [ $# -eq 0 ]; then
+  export SECRET_KEY=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | head -c 16)
+  echo "SECRET_KEY = $SECRET_KEY"
   "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:443 --certfile=cert/bookstore.pem --key=cert/bookstore.key "app:create_app()"
   "${PWD}"/venv/bin/gunicorn -D -w 2 --bind 0.0.0.0:80 "app:create_app()"
 fi
